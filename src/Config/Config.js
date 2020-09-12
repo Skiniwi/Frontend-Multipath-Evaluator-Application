b93205f57655fced5d03d515323ebe23d68ad075
skiniwi <mohamedgrira@outlook.fr>
2020-09-12 14:01:45 +0200

    Updates

--- src/Config/Config.js
+++
@@ -1,6 +0,0 @@
-const Config = {
-    "gqlBaseURL": "https://calchub.radiolab-dev0.beagile.one/query",
-    "gqlToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlcyI6WyJzZXJ2aWNlcyJdLCJ1c2VybmFtZSI6IndlYl9yYXl0cmFjZXIifQ.X-M9W-ynpsAAqPmOSlMnUeXYJ5IL3Sb6k3dSXKpXciE",
-};
-
-export default Config;
